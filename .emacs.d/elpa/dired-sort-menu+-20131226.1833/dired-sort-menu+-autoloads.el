e78d8d88addd398df5932c29f27ebdc4c5f5a47b
EDaniel <eli.daniel@gmail.com>
2014-02-25 17:59:06 -0500

    updated emacs config to 24.3 with elpa

---
+++ .emacs.d/elpa/dired-sort-menu+-20131226.1833/dired-sort-menu+-autoloads.el
@@ -0,0 +1,30 @@
+;;; dired-sort-menu+-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (dired-sort-dialogue) "dired-sort-menu+" "dired-sort-menu+.el"
+;;;;;;  (21260 64862 0 0))
+;;; Generated autoloads from dired-sort-menu+.el
+
+(autoload 'dired-sort-dialogue "dired-sort-menu+" "\
+A static dialogue version of the Dired sort menu.
+This command *must* be run in the Dired buffer!
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("dired-sort-menu+-pkg.el") (21260 64862
+;;;;;;  976116 0))
+
+;;;***
+
+(provide 'dired-sort-menu+-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dired-sort-menu+-autoloads.el ends here
---
+++ .emacs.d/elpa/font-lock-menus-20131226.1847/font-lock-menus-autoloads.el
@@ -0,0 +1,42 @@
+;;; font-lock-menus-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (flm-font-lock-fontify-more flm-font-lock-fontify-less
+;;;;;;  flm-list-fonts) "font-lock-menus" "font-lock-menus.el" (21260
+;;;;;;  64939 0 0))
+;;; Generated autoloads from font-lock-menus.el
+
+(autoload 'flm-list-fonts "font-lock-menus" "\
+List all fonts that Emacs knows about.
+
+\(fn)" t nil)
+
+(autoload 'flm-font-lock-fontify-less "font-lock-menus" "\
+Fontify the current buffer using less highlighting (decoration).
+See user option `font-lock-maximum-decoration'.
+
+\(fn)" t nil)
+
+(autoload 'flm-font-lock-fontify-more "font-lock-menus" "\
+Fontify the current buffer using more highlighting (decoration).
+See user option `font-lock-maximum-decoration'.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("font-lock-menus-pkg.el") (21260 64939
+;;;;;;  800001 0))
+
+;;;***
+
+(provide 'font-lock-menus-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; font-lock-menus-autoloads.el ends here
